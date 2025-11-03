2aa683948a923831c8f0531ac6e4c139302c478a
raymond <raymondmapayo@gmail.com>
2025-11-04 00:22:04 +0800

    Backend

--- Backend/service/ForgotEmail.js
+++ Backend/service/ForgotEmail.js
@@ -18,15 +18,20 @@ async function sendResetEmail(user, resetUrl) {
         <p>Hello ${user.fname},</p>
         <p>This is <b>${ADMIN_NAME}</b>. You requested a password reset. Please <b>Click</b> the following link to reset your password:</p>
         <p style="margin: 20px 0;">
-        <a href="${resetUrl}" style="background-color: #007BFF; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px;">>
+        <a href="${resetUrl}" style="background-color: #007BFF; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px;">
         
         Reset Password</a>
         </p>
         <b>This link expires in 1 hour</b>
-      <footer style="margin-top: 30px; font-size: 14px; color: #555;">
-  <p>This email was sent to <b>${user.email}</b> for protecting the security of your account.</p>
-  <p>Thank you for registering with ${ADMIN_NAME}.</p>
-</footer>
+    
+        <!-- Footer -->
+        <footer style="margin-top: 30px; font-size: 14px; color: #555;">
+          <p>This email was sent to <b>${
+            user.email
+          }</b> to protect your account security.</p>
+          <p>If you did not register, please ignore this email.</p>
+          <p>© ${new Date().getFullYear()} ${ADMIN_NAME}. All rights reserved.</p>
+        </footer>
       </div>
       `,
       text: `Hello ${user.fname}, you requested a password reset. Click the following link to reset your password: ${resetUrl}. This link expires in 1 hour. Thank you for using ${ADMIN_NAME}.`,
